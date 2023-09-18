3ce7bcb3760bf4b8cbd41d0c0c7b4d7759587e15
bioramonmoratori <ramon.moratori@estudante.ufjf.br>
2023-09-17 22:13:02 -0300

    CRUD de Livros e Autores

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://mywhalecom_admin:#Arnold2213@15.235.55.109:3306/mywhalecom_livrosflask'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{USER}:{PASSWORD}@15.235.55.109:3306/mywhalecom_livrosflask'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
